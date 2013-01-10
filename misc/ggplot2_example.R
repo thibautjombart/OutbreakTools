9de8849c201223163c2c3fb1a2e4090e9beecb96
olli <oliver.ratmann@imperial.ac.uk>
2013-01-10 14:53:56 +0000

    completed first ts.plot

--- misc/ggplot2_example.R
+++ misc/ggplot2_example.R
@@ -11,7 +11,11 @@ outbreakg.aes_now<- function(...)
 	structure(list(...),  class = "uneval")
 }
 
-#create mock data set
+
+#' create mock time series
+#'
+#' @return default mock time series
+#' @example x<- ts.sim();
 ts.sim<- function()
 {
 	time	<-as.Date("2013-01-01")+1:100
@@ -23,6 +27,15 @@ ts.sim<- function()
 	df_mock
 }
 
+#' create time series plot 
+#'
+#' @param df data frame with all information to be plotted
+#' @param x column name of column that should be on 'x' axis. If NULL, the first column is taken.
+#' @param y column name of column that should be on 'y' axis. If NULL, the second column is taken.
+#' @param facet column name or formula to create subplots. See examples. If NULL, no subplot is created.
+#' @param col column name to create sub time series in the same plot. See examples. If NULL, no color is used.
+#' @return ggplot2 object
+#' @example x<- ts.sim(); print(ts.plot(x,x="time",y="measure",col="sex",facet="age~school"))
 ts.plot<- function(df, x=NULL, y=NULL, facet=NULL,col=NULL)
 {
 	if(is.null(x))	x<- colnames(df)[1]
@@ -31,21 +44,21 @@ ts.plot<- function(df, x=NULL, y=NULL, facet=NULL,col=NULL)
 	if(!is.character(y))	stop("y not a character")
 	p<- ggplot(data=df,aes_string(x=x,y=y))
 	if(!is.null(facet))
-		p<- p+facet_grid(age~school)
-	
+	{
+		if(!regexpr("~",facet,fixed=1)>0)
+			facet<- paste(facet,"~.",sep='')		
+		p<- p+facet_grid(facet, scales="free")
+	}
 	if(!is.null(col))
 		p<- p+geom_line(aes_string(colour=col))
 	else
 		p<- p+geom_line()	
-	#p<-p+facet_grid(age~school)
-	#p<-p+geom_line(aes(colour=sex))
-	#print(p)
 	p
 }
 
 #main
 if(1)
 {
-	df<- ts.sim()
-	print(ts.plot(df,x="time",y="measure",col="sex"))
+	x<- ts.sim()
+	print(ts.plot(x,x="time",y="measure",col="sex",facet="age~school"))
 }
