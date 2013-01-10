99d83a9bd90525f1974607a4c5016c1ee259367e
anton camacho <tonton@dyn900-159.eduroam.ic.ac.uk>
2013-01-10 17:12:17 +0000

    add package dependency and indentation

--- pkg/R/plot_ggphy.R
+++ pkg/R/plot_ggphy.R
@@ -16,8 +16,8 @@ GeomSegment2 <- proto(ggplot2:::GeomSegment, {
                         ))
         }
     }})
-    
-    
+
+
 geom_segment2 <- function(mapping = NULL, data = NULL, stat =
                           "identity", position = "identity", arrow = NULL, ...) {
 	GeomSegment2$new(mapping = mapping, data = data, stat = stat,
@@ -27,8 +27,8 @@ geom_segment2 <- function(mapping = NULL, data = NULL, stat =
 
 plot_ggphy<-function(ggphy,tip_labels=F,tip_attribute=NULL,var_tip_labels=NULL,var_tip_colour=NULL){
 	
-	#TODO: allow edge and node attributes and merge with df_edge and df_node
-
+#TODO: allow edge and node attributes and merge with df_edge and df_node
+    
 	df_tip<-ggphy[[1]]
 	df_node<-ggphy[[2]]
 	df_edge<-ggphy[[3]]
@@ -36,38 +36,38 @@ plot_ggphy<-function(ggphy,tip_labels=F,tip_attribute=NULL,var_tip_labels=NULL,v
 	is_x_date<-inherits(df_edge$x_beg,"Date")
 	
 	if(!is.null(tip_attribute) & !is.null(var_tip_labels)){
-	#merge df_tip with tip attributes
-	tmp<-merge(df_tip,tip_attribute,by.x="label",by.y=var_tip_labels)
-	df_tip<-tmp
+#merge df_tip with tip attributes
+        tmp<-merge(df_tip,tip_attribute,by.x="label",by.y=var_tip_labels)
+        df_tip<-tmp
 	}
 	
-		
-	#theme_set(theme_grey())
-	theme_old<-theme_update( 
-	axis.ticks.y = element_blank(),
-	axis.title.y = element_blank(),	panel.grid.major.y = element_blank(),
-	panel.grid.minor.y = element_blank())
-
+    
+#theme_set(theme_grey())
+	theme_old<-theme_update(
+                            axis.ticks.y = element_blank(),
+                            axis.title.y = element_blank(),	panel.grid.major.y = element_blank(),
+                            panel.grid.minor.y = element_blank())
+    
 	p<-ggplot(df_edge)
 	p<-p+geom_segment2(data=df_edge,aes(x=x_beg,xend=x_end,y=y_beg,yend=y_end))
 	p<-p+scale_y_continuous("",labels=NULL)
 	if(is_x_date)
-	p<-p+scale_x_date("Time",labels=date_format("%Y"),minor_breaks="1 year")
-	else
-	p<-p+scale_x_continuous("Time")
-	
-	if(tip_labels){
-		p<-p+geom_text(data=df_tip,aes(x=x,y=y,label=label),hjust=0)
-	}
+        p<-p+scale_x_date("Time",labels=date_format("%Y"),minor_breaks="1 year")
+        else
+            p<-p+scale_x_continuous("Time")
+            
+            if(tip_labels){
+                p<-p+geom_text(data=df_tip,aes(x=x,y=y,label=label),hjust=0)
+            }
 	
 	if(!is.null(var_tip_colour)){
 		p<-p+geom_point(data=df_tip,aes_string(x="x",y="y",colour=var_tip_colour))
 	}
 	
 	print(p)
-
+    
 	theme_set(theme_old)
-
+    
 	return(p)	
 }
 
