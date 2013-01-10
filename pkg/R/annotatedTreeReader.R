8a685ecc66d694fa6b699bbda7bdb2bea909f086
Marc Suchard <msuchard@gmail.com>
2013-01-10 15:40:30 +0000

    First working version of annotated Newick reader

--- pkg/R/annotatedTreeReader.R
+++ pkg/R/annotatedTreeReader.R
@@ -4,39 +4,69 @@
 # A class for reading Newick formatted trees with BEAST-style annotations
 #
 
-test.MAS.function = function(x) {
-    cat(x)
+strip.annotations = function(text) {
+    annotations = list()   
+    end = 1
+    
+    pattern = "\\[&.*?\\]"
+    
+    repeat {
+        match = regexpr(pattern=pattern,text=text)
+        if (!(match[1] > 0)) {
+            break
+        }                
+        annotations[[end]] = regmatches(text, match)
+        text = sub(pattern,paste("[",end,"]",sep=""), text)
+        end = end + 1
+    }
+    return(list(annotations=annotations,tree=text))
 }
 
-# THE CODE BELOW COMES FROM 'ape'. MY GOAL IS TO DERIVE FROM THIS TO READ IN BEAST-STYLE ANNOTATIONS
+split.tree.names = function(text, header=FALSE) {
+    if (header) {
+      text = gsub(pattern="\\{",x=text,replacement="\\[")
+      text = gsub(pattern="\\}",x=text,replacement="\\]")
+    }
+    text = gsub(pattern="\\[.*?\\]=",x=text,replacement="")
+    text = gsub(pattern="^tree",x=text,replacement="")
+    return(text)
+}
 
-parse.traits = function(text, header=FALSE) {
+split.tree.traits = function(text, header=FALSE) {
 
     if (header) {
       text = gsub(pattern="\\{",x=text,replacement="\\[")
       text = gsub(pattern="\\}",x=text,replacement="\\]")
     }
-  
+
   # Pull out annotation
   text = regmatches(text,regexpr(pattern="\\[.*?\\]",text))
   # Remove leading and trailing delimitors
   text = substring(text,3,nchar(text)-1)  
+  return(text)
+}
+
+parse.traits = function(text, header=FALSE) {
+
+    if (header == TRUE) {
+        text = substring(text,3,nchar(text)-1)
+    }
 
   pattern = "(\"[^\"]*\"+|[^,=\\s]+)\\s*(=\\s*(\\{[^=]*\\}|\"[^\"]*\"+|[^,]+))?"
   
   rgx = gregexpr(pattern,text,perl=TRUE)
   n = length(attr(rgx[[1]],"match.length"))
   traits = list()
-  start = attr(rgx[[1]],"capture.start")
-  names = attr(rgx[[1]],"capture.names")
+  start  = attr(rgx[[1]],"capture.start")
+  names  = attr(rgx[[1]],"capture.names")
   length = attr(rgx[[1]],"capture.length")
-  names = attr(rgx[[1]],"capture.names")
+  names  = attr(rgx[[1]],"capture.names")
   for (i in 1:n) {
     s = start[i,3]
-    e = s + length[i,3]
-    if (substring(text,e,e) == ",") {
-        e = e - 1
-    }
+    e = s + length[i,3] - 1
+#    if (substring(text,e,e) == ",") {
+#        e = e - 1
+#    }
     value = substring(text,s,e)    
     
     s = start[i,1]
@@ -45,16 +75,129 @@ parse.traits = function(text, header=FALSE) {
         e = e - 1
     }
     key = substring(text,s,e)
+    
+    ### TODO: Delegate below to a more generic parser
+    ### TODO: Need arrays, boolean
     if (!is.na(as.numeric(value))) {
         value = as.numeric(value)
     }
     traits[[key]] = value
   }
 
- #   browser()
- 
- print(traits)
+#  browser() 
+#  print(traits)
     return(traits)
+}
+
+
+# THE CODE BELOW COMES FROM 'ape'. MY GOAL IS TO DERIVE FROM THIS TO READ IN BEAST-STYLE ANNOTATIONS
+
+
+MAS.tree.build =
+function (tp) {
+    
+    add.internal <- function() {
+        edge[j, 1] <<- current.node
+        edge[j, 2] <<- current.node <<- node <<- node + 1L
+        index[node] <<- j
+        j <<- j + 1L
+    }
+    add.terminal <- function() {
+        edge[j, 1] <<- current.node
+        edge[j, 2] <<- tip
+        index[tip] <<- j
+        X <- unlist(strsplit(tpc[k], ":"))
+        tip.label[tip] <<- X[1]
+        edge.length[j] <<- as.numeric(X[2])
+        k <<- k + 1L
+        tip <<- tip + 1L
+        j <<- j + 1L
+    }
+    go.down <- function() {
+        l <- index[current.node]
+        X <- unlist(strsplit(tpc[k], ":"))
+        node.label[current.node - nb.tip] <<- X[1]
+        edge.length[l] <<- as.numeric(X[2])
+        k <<- k + 1L
+        current.node <<- edge[l, 1]
+    }
+    if (!length(grep(",", tp))) {
+        obj <- list(edge = matrix(c(2L, 1L), 1, 2))
+        tp <- unlist(strsplit(tp, "[\\(\\):;]"))
+        obj$edge.length <- as.numeric(tp[3])
+        obj$Nnode <- 1L
+        obj$tip.label <- tp[2]
+        if (tp[4] != "") 
+            obj$node.label <- tp[4]
+        class(obj) <- "phylo"
+        return(obj)
+    }
+    
+    result = strip.annotations(tp)
+    annotations = result$annotations
+    new.tp.stripped = result$tree
+    
+    annotations = lapply(annotations, parse.traits, header=TRUE)
+    
+#    browser()
+    
+    tp.stripped = gsub("\\[.*?\\]","",tp)
+    tpc <- unlist(strsplit(tp.stripped, "[\\(\\),;]"))
+    tpc <- tpc[nzchar(tpc)]
+    
+    tsp <- unlist(strsplit(tp.stripped, NULL))
+    skeleton <- tsp[tsp %in% c("(", ")", ",", ";")]
+    nsk <- length(skeleton)
+    nb.node <- sum(skeleton == ")")
+    nb.tip <- sum(skeleton == ",") + 1
+    nb.edge <- nb.node + nb.tip
+    
+#    browser()
+    
+    node.label <- character(nb.node)
+    tip.label <- character(nb.tip)
+    edge.length <- numeric(nb.edge)
+    edge <- matrix(0L, nb.edge, 2)
+    current.node <- node <- as.integer(nb.tip + 1)
+    edge[nb.edge, 2] <- node
+    index <- numeric(nb.edge + 1)
+    index[node] <- nb.edge
+    j <- k <- tip <- 1L
+    for (i in 2:nsk) {
+        if (skeleton[i] == "(") 
+            add.internal()
+        if (skeleton[i] == ",") {
+            if (skeleton[i - 1] != ")") 
+                add.terminal()
+        }
+        if (skeleton[i] == ")") {
+            if (skeleton[i - 1] == ",") {
+                add.terminal()
+                go.down()
+            }
+            if (skeleton[i - 1] == ")") 
+                go.down()
+        }
+    }
+    edge <- edge[-nb.edge, ]
+    obj <- list(edge = edge, Nnode = nb.node, tip.label = tip.label)
+    root.edge <- edge.length[nb.edge]
+    edge.length <- edge.length[-nb.edge]
+    if (!all(is.na(edge.length))) 
+        obj$edge.length <- edge.length
+    if (is.na(node.label[1])) 
+        node.label[1] <- ""
+    if (any(nzchar(node.label))) 
+        obj$node.label <- node.label
+    if (!is.na(root.edge)) 
+        obj$root.edge <- root.edge
+    class(obj) <- "phylo"
+    attr(obj, "order") <- "cladewise"
+    
+    obj$annotations = annotations
+#    obj$tmp = new.tp.stripped
+    
+    obj
 }
 
 MAS.read.tree = function (file = "", text = NULL, tree.names = NULL, skip = 0, 
@@ -86,6 +229,7 @@ MAS.read.tree = function (file = "", text = NULL, tree.names = NULL, skip = 0,
     }
     
     tree <- gsub("[ \n\t]", "", tree)
+    tree <- gsub("\\[&R\\]", "", tree)
     tree <- unlist(strsplit(tree, NULL))
     y <- which(tree == ";")
     Ntree <- length(y)
@@ -104,20 +248,26 @@ MAS.read.tree = function (file = "", text = NULL, tree.names = NULL, skip = 0,
     colon <- grep(":", STRING)   
     
     if (!is.null(tree.names)) {
-        tree.traits = lapply(tree.names, parse.traits, header=T)
+        traits.text = lapply(tree.names, split.tree.traits, header=T)
+        tree.names = lapply(tree.names, split.tree.names, header=T)
+        tree.traits = lapply(traits.text, parse.traits)
     }
     
-        browser()
+    lapply(STRING, check.magic.character, magic.character="\\|")
+    lapply(STRING, convert.to.magic.character, magic.character="\\|")
+    
+#        browser()
     
     if (!length(colon)) {
-        obj <- lapply(STRING, clado.build)
+        stop(paste("Annotated clado.build is not yet implemented.\n"))
+        obj <- lapply(STRING, MAS.clado.build)
     }
     else if (length(colon) == Ntree) {
-        obj <- lapply(STRING, tree.build)
+        obj <- lapply(STRING, MAS.tree.build)
     }
     else {
         obj <- vector("list", Ntree)
-        obj[colon] <- lapply(STRING[colon], tree.build)
+        obj[colon] <- lapply(STRING[colon], MAS.tree.build)
         nocolon <- (1:Ntree)[!1:Ntree %in% colon]
         obj[nocolon] <- lapply(STRING[nocolon], clado.build)
     }
@@ -131,8 +281,9 @@ MAS.read.tree = function (file = "", text = NULL, tree.names = NULL, skip = 0,
     if (Ntree == 1 && !keep.multi) 
         obj <- obj[[1]]
     else {
-        if (!is.null(tree.names)) 
+        if (!is.null(tree.names)) {
             names(obj) <- tree.names
+        }
         class(obj) <- "multiPhylo"
     }
     obj
