a75313094105da60134d0209ab7402246dda7fb4
olli <oliver.ratmann@imperial.ac.uk>
2013-01-10 14:22:01 +0000

    updated ts.plot

--- misc/ggplot2_example.R
+++ misc/ggplot2_example.R
@@ -6,6 +6,11 @@ library(reshape2)
 #source(ggplot2_example.R)
 
 
+outbreakg.aes_now<- function(...) 
+{
+	structure(list(...),  class = "uneval")
+}
+
 #create mock data set
 ts.sim<- function()
 {
@@ -18,16 +23,29 @@ ts.sim<- function()
 	df_mock
 }
 
-ts.plot(df, x=NULL, y=NULL, facet=NULL,col=NULL)
+ts.plot<- function(df, x=NULL, y=NULL, facet=NULL,col=NULL)
 {
 	if(is.null(x))	x<- colnames(df)[1]
 	if(is.null(y))	y<- colnames(df)[2]
 	if(!is.character(x))	stop("x not a character")
 	if(!is.character(y))	stop("y not a character")
-	p<- ggplot(data=df,aes(x=x,y=y))
+	p<- ggplot(data=df,aes_string(x=x,y=y))
+	if(!is.null(facet))
+		p<- p+facet_grid(age~school)
+	
+	if(!is.null(col))
+		p<- p+geom_line(aes_string(colour=col))
+	else
+		p<- p+geom_line()	
 	#p<-p+facet_grid(age~school)
 	#p<-p+geom_line(aes(colour=sex))
 	#print(p)
 	p
 }
 
+#main
+if(1)
+{
+	df<- ts.sim()
+	print(ts.plot(df,x="time",y="measure",col="sex"))
+}
