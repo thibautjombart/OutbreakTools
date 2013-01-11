a90f1abbc1b7e8ea1db75fb55964d0bdf1ce9f05
Marc Suchard <msuchard@gmail.com>
2013-01-11 10:39:23 +0000

    Initial import of a NEXUS file reader for annotated trees; still do not function properly.

--- pkg/R/annotatedTreeReader.R
+++ pkg/R/annotatedTreeReader.R
@@ -79,10 +79,6 @@ parse.value = function(text) {
     return(value)
 }
 
-
-
-     
-
 parse.traits = function(text, header=FALSE) {
 
     if (header == TRUE) {
@@ -300,3 +296,147 @@ MAS.read.tree = function (file = "", text = NULL, tree.names = NULL, skip = 0,
     }
     obj
 }
+
+MAS.read.nexus = function (file, tree.names = NULL) {
+    X <- scan(file = file, what = "", sep = "\n", quiet = TRUE)
+    LEFT <- grep("\\[", X)
+    RIGHT <- grep("\\]", X)
+    if (length(LEFT)) {
+        w <- LEFT == RIGHT
+        if (any(w)) {
+            s <- LEFT[w]
+            X[s] <- gsub("\\[[^]]*\\]", "", X[s])
+        }
+        w <- !w
+        if (any(w)) {
+            s <- LEFT[w]
+            X[s] <- gsub("\\[.*", "", X[s])
+            sb <- RIGHT[w]
+            X[sb] <- gsub(".*\\]", "", X[sb])
+            if (any(s < sb - 1)) 
+                X <- X[-unlist(mapply(":", (s + 1), (sb - 1)))]
+        }
+    }
+    endblock <- grep("END;|ENDBLOCK;", X, ignore.case = TRUE)
+    semico <- grep(";", X)
+    i1 <- grep("BEGIN TREES;", X, ignore.case = TRUE)
+    i2 <- grep("TRANSLATE", X, ignore.case = TRUE)
+    translation <- if (length(i2) == 1 && i2 > i1) 
+        TRUE
+    else FALSE
+    if (translation) {
+        end <- semico[semico > i2][1]
+        x <- X[(i2 + 1):end]
+        x <- unlist(strsplit(x, "[,; \t]"))
+        x <- x[nzchar(x)]
+        TRANS <- matrix(x, ncol = 2, byrow = TRUE)
+        TRANS[, 2] <- gsub("['\"]", "", TRANS[, 2])
+        n <- dim(TRANS)[1]
+    }
+    start <- if (translation) 
+        semico[semico > i2][1] + 1
+    else semico[semico > i1][1]
+    end <- endblock[endblock > i1][1] - 1
+    tree <- X[start:end]
+    rm(X)
+    tree <- tree[tree != ""]
+    semico <- grep(";", tree)
+    Ntree <- length(semico)
+    if (Ntree == 1 && length(tree) > 1) 
+        STRING <- paste(tree, collapse = "")
+    else {
+        if (any(diff(semico) != 1)) {
+            STRING <- character(Ntree)
+            s <- c(1, semico[-Ntree] + 1)
+            j <- mapply(":", s, semico)
+            if (is.list(j)) {
+                for (i in 1:Ntree) STRING[i] <- paste(tree[j[[i]]], 
+                  collapse = "")
+            }
+            else {
+                for (i in 1:Ntree) STRING[i] <- paste(tree[j[, 
+                  i]], collapse = "")
+            }
+        }
+        else STRING <- tree
+    }
+    rm(tree)
+    STRING <- STRING[grep("^[[:blank:]]*tree.*= *", STRING, ignore.case = TRUE)]
+    Ntree <- length(STRING)
+    nms.trees <- sub(" *= *.*", "", STRING)
+    nms.trees <- sub("^ *tree *", "", nms.trees, ignore.case = TRUE)
+    STRING <- sub("^.*= *", "", STRING)
+    STRING <- gsub(" ", "", STRING)
+    colon <- grep(":", STRING)
+    if (!length(colon)) {
+        trees <- lapply(STRING, clado.build)
+    }
+    else if (length(colon) == Ntree) {
+        trees <- if (translation) {
+            stop("treeBuildWithTokens is not yet implemented.\n")
+            lapply(STRING, .treeBuildWithTokens)
+        }
+        else lapply(STRING, MAS.tree.build)
+    }
+    else {
+        trees <- vector("list", Ntree)
+        trees[colon] <- lapply(STRING[colon], MAS.tree.build)
+        nocolon <- (1:Ntree)[!1:Ntree %in% colon]
+        trees[nocolon] <- lapply(STRING[nocolon], MAS.clado.build)
+        if (translation) {
+            for (i in 1:Ntree) {
+                tr <- trees[[i]]
+                for (j in 1:n) {
+                  ind <- which(tr$tip.label[j] == TRANS[, 1])
+                  tr$tip.label[j] <- TRANS[ind, 2]
+                }
+                if (!is.null(tr$node.label)) {
+                  for (j in 1:length(tr$node.label)) {
+                    ind <- which(tr$node.label[j] == TRANS[, 
+                      1])
+                    tr$node.label[j] <- TRANS[ind, 2]
+                  }
+                }
+                trees[[i]] <- tr
+            }
+            translation <- FALSE
+        }
+    }
+    for (i in 1:Ntree) {
+        tr <- trees[[i]]
+        if (!translation) 
+            n <- length(tr$tip.label)
+        ROOT <- n + 1
+        if (sum(tr$edge[, 1] == ROOT) == 1 && dim(tr$edge)[1] > 
+            1) {
+            stop(paste("The tree has apparently singleton node(s): cannot read tree file.\n  Reading NEXUS file aborted at tree no.", 
+                i, sep = ""))
+        }
+    }
+    if (Ntree == 1) {
+        trees <- trees[[1]]
+        if (translation) {
+            trees$tip.label <- if (length(colon)) 
+                TRANS[, 2]
+            else TRANS[, 2][as.numeric(trees$tip.label)]
+        }
+    }
+    else {
+        if (!is.null(tree.names)) 
+            names(trees) <- tree.names
+        if (translation) {
+            if (length(colon) == Ntree) 
+                attr(trees, "TipLabel") <- TRANS[, 2]
+            else {
+                for (i in 1:Ntree) trees[[i]]$tip.label <- TRANS[, 
+                  2][as.numeric(trees[[i]]$tip.label)]
+                trees <- .compressTipLabel(trees)
+            }
+        }
+        class(trees) <- "multiPhylo"
+        if (!all(nms.trees == "")) 
+            names(trees) <- nms.trees
+    }
+    trees
+}
+
