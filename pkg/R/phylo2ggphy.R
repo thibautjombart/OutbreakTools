06ff137abe28a0de6aed92449f1c7e28beff28c9
Thibaut Jombart <tjombart@imperial.ac.uk>
2013-02-19 18:56:32 +0000

    Replacing _ by . in code; fixed order in which functions are defined in  plotIndividualTimeline

--- pkg/R/phylo2ggphy.R
+++ pkg/R/phylo2ggphy.R
@@ -1,154 +1,154 @@
 #' Function to convert phylogenies from the class 'phylo' to the class 'ggphy'
 #'
 #' @param phylo an object of the class "phylo"
-#' @param tip_dates a vector containing the sample dates of the tip in "Date" format, the dates must be ordered like the tips
-#' @param branch_unit the unit of the branch. Either "year", "month", "day" or "subst". If a time unit is provided, together with tip_dates, then the x-axis of the phylogeny will be in the Date format 
+#' @param tip.dates a vector containing the sample dates of the tip in "Date" format, the dates must be ordered like the tips
+#' @param branch.unit the unit of the branch. Either "year", "month", "day" or "subst". If a time unit is provided, together with tip.dates, then the x-axis of the phylogeny will be in the Date format
 #' @export
 #' @author Anton Camacho
-#' @examples see misc/plot_ggphy_test.R
+#' @examples see misc/plot.ggphy.test.R
 
-phylo2ggphy<-function(phylo,tip_dates=NULL,branch_unit="subst"){
-	
+phylo2ggphy<-function(phylo,tip.dates=NULL,branch.unit="subst"){
+
 	phy<-phylo
-	has_node_label<-(!is.null(phy$node.label))
-	
-	N_tips<-length(phy$tip.label)
-    if(!is.null(tip_dates) & length(tip_dates)!=N_tips){stop("tip_dates must be the same size as the number of tips if provided")}
-    
+	has.node.label<-(!is.null(phy$node.label))
+
+	N.tips<-length(phy$tip.label)
+    if(!is.null(tip.dates) & length(tip.dates)!=N.tips){stop("tip.dates must be the same size as the number of tips if provided")}
+
 	edge<-as.data.frame(phy$edge)
 	names(edge)<-c("beg","end")
-	
+
 	#if edge.length is not provided, set all to 1 by default
 	if(is.null(edge$length<-phy$edge.length)){edge$length<-rep(1,nrow(edge))}
-	
-    
+
+
 #find root
 	ind<-which(!edge$beg%in%edge$end)
-	phy_root<-unique(edge$beg[ind])
-	if(length(phy_root)!=1){
-        cat(length(phy_root),"root(s) found!!\n")
+	phy.root<-unique(edge$beg[ind])
+	if(length(phy.root)!=1){
+        cat(length(phy.root),"root(s) found!!\n")
         stop("Algorithm cannot handle more than one root in the phylo at the moment!")
 	}
-    
+
 	if(!is.rooted(phy)){
-		
+
 #find outgroup
-#ind<-which(edge$beg==phy_root & edge$end<=N_tips)
+#ind<-which(edge$beg==phy.root & edge$end<=N.tips)
 #outG<-edge$end[ind]
-		phy<-root(phy,node=phy_root,r=T)
+		phy<-root(phy,node=phy.root,resolve.root=TRUE)
 #remove outG label
 #phy$tip.label<-phy$tip.label[-outG]
-		
-		if(has_node_label){
+
+		if(has.node.label){
 #add NA to the root label
-            ind<-phy_root-N_tips
+            ind<-phy.root-N.tips
             tmp<-rep(NA,phy$Nnode)
             tmp[-ind]<-phy$node.label
             phy$node.label<-tmp
 		}
 	}
-    
-	Nt<-N_tips
+
+	Nt<-N.tips
 	Nn<-phy$Nnode
 	Ne<-nrow(phy$edge)
 	Nnt<-Nn+Nt
-	
+
 	tip<-1:Nt
 	node<-(Nt+1):Nnt
-    
+
 #get the y coord of nodes: start from tips and browse tree to the root
 	y<-rep(NA,Nnt)
-	order_tip<-edge$end[edge$end<=Nt]
-	y[order_tip]<-tip
-	cur_tip<-tip
+	order.tip<-edge$end[edge$end<=Nt]
+	y[order.tip]<-tip
+	cur.tip<-tip
 	removed<-c()
-	
+
 	while(length(removed)<(Nnt-1)){
-		
-#how many nodes have two children among cur_tip
-		ind<-which(edge$end%in%cur_tip)
-		n_children<-table(edge$beg[ind])
-		ind<-which(n_children>1)
-		
-		new_tip<-as.numeric(names(ind))
-		old_tip<-edge$end[edge$beg%in%new_tip]
-		
-		y_new_tip<-sapply(new_tip,function(i){
+
+#how many nodes have two children among cur.tip
+		ind<-which(edge$end%in%cur.tip)
+		n.children<-table(edge$beg[ind])
+		ind<-which(n.children>1)
+
+		new.tip<-as.numeric(names(ind))
+		old.tip<-edge$end[edge$beg%in%new.tip]
+
+		y.new.tip<-sapply(new.tip,function(i){
 			children<-edge$end[edge$beg==i]
 			return(mean(y[children]))
 		})
-		
-		y[new_tip]<-y_new_tip
-		removed<-c(removed,old_tip)
-		cur_tip<-cur_tip[!cur_tip%in%old_tip]
-		cur_tip<-c(cur_tip,new_tip)
-		
+
+		y[new.tip]<-y.new.tip
+		removed<-c(removed,old.tip)
+		cur.tip<-cur.tip[!cur.tip%in%old.tip]
+		cur.tip<-c(cur.tip,new.tip)
+
 	}
-	
+
 #get x coord: start from root and browse tree until tips
 	x<-rep(NA,Nnt)
-	x[phy_root]<-0
-	cur_node<-phy_root
-	visited<-c(cur_node)
-    
+	x[phy.root]<-0
+	cur.node<-phy.root
+	visited<-c(cur.node)
+
 	while(length(visited)<Nnt){
-		df1<-data.frame(beg=cur_node)
+		df1<-data.frame(beg=cur.node)
 		df2<-merge(df1,edge)
 		x[df2$end]<-x[df2$beg]+df2$length
-		cur_node<-df2$end
-		visited<-c(visited,cur_node)
+		cur.node<-df2$end
+		visited<-c(visited,cur.node)
 	}
-	
-	
-    if(!is.null(tip_dates) & branch_unit%in%c("year","month","day")){
+
+
+    if(!is.null(tip.dates) & branch.unit%in%c("year","month","day")){
         cat("X axis is converted into date\n")
-#tip_date<-as.Date(extract_string(phy$tip.label,"_",2))
-        df<-data.frame(tip,date=tip_dates,age=x[tip])
-        time_unit=switch(branch_unit,year=365.25,month=30.5,day=1)
-        root_date<-mean(df$date-df$age*time_unit)
+#tip.date<-as.Date(extract.string(phy$tip.label,".",2))
+        df<-data.frame(tip,date=tip.dates,age=x[tip])
+        time.unit=switch(branch.unit,year=365.25,month=30.5,day=1)
+        root.date<-mean(df$date-df$age*time.unit)
 #check that the new tip dates do not vary much from sample dates
-#new_tip_date<-root_date+df$age*time_unit
-#print(range(new_tip_date-df$date))
+#new.tip.date<-root.date+df$age*time.unit
+#print(range(new.tip.date-df$date))
 #x coord as date
-        x<-root_date+x*time_unit
+        x<-root.date+x*time.unit
     }
-    
-    
+
+
 #build data frame for nodes and tips
-    if(has_node_label){
-        df_node<-data.frame(node,x=x[node],y=y[node],label=phy$node.label)
+    if(has.node.label){
+        df.node<-data.frame(node,x=x[node],y=y[node],label=phy$node.label)
     }else{
-        df_node<-data.frame(node,x=x[node],y=y[node])
+        df.node<-data.frame(node,x=x[node],y=y[node])
     }
-    
-    df_tip<-data.frame(tip,x=x[tip],y=y[tip],label=phy$tip.label)
-    
+
+    df.tip<-data.frame(tip,x=x[tip],y=y[tip],label=phy$tip.label)
+
 #build segment data frame
 #horizontal segments
-    edge_h<-edge
-    edge_h$direction<-"H"
-    edge_h$x_beg<-x[edge_h$beg]
-    edge_h$x_end<-x[edge_h$end]
-    edge_h$y_beg<-y[edge_h$end]
-    edge_h$y_end<-edge_h$y_beg
-    
+    edge.h<-edge
+    edge.h$direction<-"H"
+    edge.h$x.beg<-x[edge.h$beg]
+    edge.h$x.end<-x[edge.h$end]
+    edge.h$y.beg<-y[edge.h$end]
+    edge.h$y.end<-edge.h$y.beg
+
 #vertical segments
-    edge_v<-edge
-    edge_v$direction<-"V"
-    edge_v$x_beg<-x[edge_v$beg]
-    edge_v$x_end<-edge_v$x_beg
-    edge_v$y_beg<-y[edge_v$beg]
-    edge_v$y_end<-y[edge_v$end]
-    
+    edge.v<-edge
+    edge.v$direction<-"V"
+    edge.v$x.beg<-x[edge.v$beg]
+    edge.v$x.end<-edge.v$x.beg
+    edge.v$y.beg<-y[edge.v$beg]
+    edge.v$y.end<-y[edge.v$end]
+
 #combine
-    df_edge<-rbind(edge_h,edge_v)
-    df_edge<-df_edge[,-which(names(df_edge)=="length")]
-    
+    df.edge<-rbind(edge.h,edge.v)
+    df.edge<-df.edge[,-which(names(df.edge)=="length")]
+
 #if node.label!=NULL put label on edges
-    if(has_node_label){
+    if(has.node.label){
         tmp<-data.frame(end=node,label=phy$node.label)
-        df_edge<-merge(df_edge,tmp,all.x=T)
+        df.edge<-merge(df.edge,tmp,all.x=TRUE)
     }
-    
-    return(list(df_tip,df_node,df_edge))
+
+    return(list(df.tip,df.node,df.edge))
 }
