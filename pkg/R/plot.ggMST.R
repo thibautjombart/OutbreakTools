c7e13efae948ed3791807eaba5e8c40f33404279
Thibaut Jombart <tjombart@imperial.ac.uk>
2013-02-21 17:51:43 +0000

    sorting out naming of plotggMST - not a method

--- pkg/R/plot.ggMST.R
+++ pkg/R/plot.ggMST.R
@@ -1,20 +1,20 @@
 #' Function to plot a minimum spanning tree using ggplot from a the class 'uniqSequences'
 #'
-#' @param x an object of the class "uniqSequences"
+#' @param uniqdna an object of the class "uniqSequences"
 #' @export
 #' @author Joseph Hughes
 #' @examples see below
 
-plot.ggMST<-function(x, y=NULL, ...){
-  if (!is.null(x) && inherits(x,"uniqSequences")){
+plotggMST<-function(uniqdna){
+  if (!is.null(uniqdna) && inherits(uniqdna,"uniqSequences")){
     # get the counts for each sequence
-    IDcounts<-do.call(rbind, lapply(x@uniqID, function(x) length(x)))
+    IDcounts<-do.call(rbind, lapply(uniqdna@uniqID, function(x) length(x)))
     IDcounts<-as.data.frame(IDcounts[order(-IDcounts[,1]),])
-    colnames(IDcounts) <- c( 'count')
+    colnames(IDcounts) <- c( 'count') 
     # print(IDcounts)
-    seqindex<-match(rownames(IDcounts), labels(x@x))
+    seqindex<-match(rownames(IDcounts), labels(uniqdna@uniqdna))
     # reorder the DNAbin accordingly
-    ordereddna<-x@x[seqindex, ]
+    ordereddna<-uniqdna@uniqdna[seqindex, ]
     # print(ordereddna)
     uniqdist<-dist.dna(ordereddna,model="raw", as.matrix=TRUE)
     mstdist<-mst(uniqdist)
@@ -44,16 +44,16 @@ plot.ggMST<-function(x, y=NULL, ...){
     # edges of zero are removed
     #emst<-subset(eList,eList[,3]>0)
     edges <- data.frame(plotcord[eList[,1],1:2], plotcord[eList[,2],1:2])
-
+    
     colnames(edges) <-  c("X1","Y1","X2","Y2")
     # print(edges)
-
+    
     old <- theme_set(theme_bw())
     old<-theme_update(
                axis.ticks.y = element_blank(), axis.ticks.x = element_blank(),
                axis.title.y = element_blank(),	panel.grid.major.y = element_blank(),
                panel.grid.minor.y = element_blank(), panel.grid.major.x = element_blank(),
-               panel.grid.minor.x = element_blank(), panel.border = element_blank(),
+               panel.grid.minor.x = element_blank(), panel.border = element_blank(), 
                panel.background = element_blank(), legend.position = "none")
 
     pmst<-ggplot()
@@ -81,9 +81,9 @@ plot.ggMST<-function(x, y=NULL, ...){
 ## get a particular sequence id with summary.seq$uniqseqID4[5]
 ## get number of sequences length(summary.seq$uniqseqID4)
 ## uniq904<-dna2uniqSequences(subsetdna)
-## plot.ggMST(uniq904)
+## plot_ggMST(uniq904)
 
 ##this below will take a long time
-## x<-dna2uniqSequences(dna)
-## plot.ggMST(x)
+## uniqdna<-dna2uniqSequences(dna)
+## plot_ggMST(uniqdna)
 
