efa008552a27fd02b04b5f674039033b96b275b3
Thibaut Jombart <tjombart@imperial.ac.uk>
2013-03-01 12:32:07 +0000

    first draft of show

--- pkg/R/obkData_basics.R
+++ pkg/R/obkData_basics.R
@@ -4,15 +4,29 @@
 ########################
 
 
-#############
+##########
 ## show ##
-#############
+##########
 
-#setMethod ("show", "obkData", function(x){
-# 	for (n in 1:6){
-#  	 print(slotNames(x)[n],quote=FALSE)
-#	show(slot(x,slotNames(x)[n]))
-# })
+setMethod("show", "obkData", function(object){
+    N <- length(slotNames(object))
+    cat("\n== obkData object ==")
+    empty <- rep(TRUE, N)
+    for(i in 1:N){
+        if(!is.null(slot(object, slotNames(object)[i]))){
+            cat(paste("\n@", slotNames(object)[i], "\n",sep=""))
+            print(slot(object, slotNames(object)[i]))
+            empty[i] <- FALSE
+        }
+    }
+
+    if(any(empty)){
+        txt <- paste("@", slotNames(object)[empty], collapse=", ", sep="")
+        cat("\nEmpty slots:", txt)
+    }
+
+    cat("\n")
+})
 
 
 #############
