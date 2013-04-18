7c19695ce7423484a4293d192cca2fa621789292
Anton <ntncmch@gmail.com>
2013-04-18 09:01:24 +0100

    edited this test function

--- misc/plot_ggphy_test.R
+++ misc/plot_ggphy_test.R
@@ -1,6 +1,11 @@
-                                        ##source("/Users/tonton/Documents/Project/Rfiles/Utils.R")
-                                        ##library(ggplot2)
-                                        ##library(scales)
+source("/Users/tonton/Documents/Project/Rfiles/Utils.R")
+library(ggplot2)
+library(scales)
+setwd("/Users/tonton/Documents/GitProjects/epibase/code/misc")
+source("/Users/tonton/Documents/GitProjects/epibase/code/pkg/R/phylo2ggphy.R")
+source("/Users/tonton/Documents/GitProjects/epibase/code/pkg/R/plotggphy.R")
+
+library(epibase)
 
 test<-function(){
 
@@ -9,15 +14,19 @@ test<-function(){
                                         #create a phylo
     my_phylo<-read.nexus(file=paste(dir,file,sep="/"))
     phylo<-ladderize(my_phylo)
+    
+    p<-epibase:::plotggphy(phylo,branch.unit="year",guess.tip.dates.from.labels=T,set.guess=list(prefix="_",order=2))
+    
     tip_dates<-as.Date(extract_string(phylo$tip.label,"_",2))
-    my_ggphy<-phylo2ggphy(phylo,tip_dates=tip_dates,branch_unit="year")
+    tip_attribute<-data.frame(label=phylo$tip.labe,tip.dates=tip_dates,tip.col=1:length(tip_dates))
+    p<-epibase:::plotggphy(phylo,branch.unit="year",tip.attribute=tip_attribute,tip.colour="tip.col",tip.dates="tip.dates")
     
-    tip_attributes<-data.frame(tip_id=phylo$tip.label,tip_colour=as.factor(sample(1:5,size=length(tip_dates),replace=T)))
-    
-    p<-plot_ggphy(my_ggphy,tip_labels=F)
-    
-    
-    p<-plot_ggphy(my_ggphy,tip_labels=F,tip_attribute=tip_attributes,var_tip_labels="tip_id",var_tip_colour="tip_colour")
+    #H1N1 example
+    phylo<-read.nexus("../../pandemic_geo.mcc")
+    location<-.extract.string(phylo$tip.label,"_",2,from="last")        
+    tip_attribute<-data.frame(label=phylo$tip.labe,location=location,dummy_shape=sample(LETTERS[1:3],length(location),replace=T))
+    p<-plotggphy(phylo,ladderize=T,branch.unit="year",tip.attribute=tip_attribute,tip.colour="location",tip.size=3,tip.shape="dummy_shape",tip.alpha=0.75,guess.tip.dates.from.labels=T,set.guess=list(prefix="_",order=1,from="last"),axis.date.format="%b%Y",major.breaks=NULL,minor.breaks="months",colour.palette="Spectral",legend.position="right")
+
     
     load("../pkg/data/singapore.Rdata")	
     my_ggphy2<-phylo2ggphy(trees)
