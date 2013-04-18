4975256c62892f2813e01aecc28a780e0eb47bff
jombart <t.jombart@imperial.ac.uk>
2013-04-18 16:17:07 +0700

    Fixed .process.Date

--- pkg/R/utils.R
+++ pkg/R/utils.R
@@ -31,47 +31,45 @@
 ###################
 ## .process.Date ##
 ###################
+
 .process.Date <- function(x, format=NULL){
+    if(inherits(x, "Date")) return(x)
 
-  date.format <- format
+    if(is.null(format)){
 
-  if(is.null(date.format)){
+        bshape <- 0
+        if(is.factor(x)) x <- as.character(x)
+        x1 <- x[1]
+        symb <- unlist(strsplit(gsub("[[:digit:]]","", x1),""))[1L]
+        temp <- unlist(strsplit(x1, paste("[",symb,"]",sep="")))
 
-    bshape <- 0
-    if(is.factor(x)) x <- as.character(x)
-    x1 <- x[1]
-    symb <- unlist(strsplit(gsub("[[:digit:]]","", x1),""))[1L]
-    temp <- unlist(strsplit(x1, paste("[",symb,"]",sep="")))
+        if(nchar(temp)[2]>2){
+            ## months are written in letters
+            ## needs to make sure the locale is fine
+            bshape <- 1
+            lct <- Sys.getlocale("LC_TIME"); Sys.setlocale("LC_TIME", "C")
 
-    if(nchar(temp)[2]>2){
-        ## months are written in letters
-        ## needs to make sure the locale is fine
-        bshape <- 1
-        lct <- Sys.getlocale("LC_TIME"); Sys.setlocale("LC_TIME", "C")
+            on.exit(Sys.setlocale("LC_TIME", lct))
+        }
 
-        on.exit(
-                Sys.setlocale("LC_TIME", lct)
-                )
+        if(nchar(temp)[1]==4){
+            if(bshape)
+                format <- paste("%Y","%b","%d", sep=symb)
+            else
+                format <- paste("%Y","%m","%d", sep=symb)
+        }
+        else if(nchar(temp)[3]==4){
+            if(bshape)
+                format <- paste("%d","%b","%Y", sep=symb)
+            else
+                format <- paste("%d","%m","%Y", sep=symb)
+        }
+        else{
+            format <- ""
+            warning(paste("date is provided in an ambiguous format\n"))
+            return(as.Date(rep(NA, length(x))))
+        }
     }
 
-    if(nchar(temp)[1]==4){
-        if(bshape)
-            date.format <- paste("%Y","%b","%d", sep=symb)
-        else
-            date.format <- paste("%Y","%m","%d", sep=symb)
-    }
-    else if(nchar(temp)[3]==4){
-        if(bshape)
-            date.format <- paste("%d","%b","%Y", sep=symb)
-        else
-            date.format <- paste("%d","%m","%Y", sep=symb)
-    }
-    else{
-        date.format=""
-        warning(paste("date is provided in an ambiguous format\n"))
-        return(as.Date(rep(NA, length(x))))
-    }
-}
-
-  return(as.Date(x,format=date.format))
+    return(as.Date(x,format=format))
 } # end .process.Date
