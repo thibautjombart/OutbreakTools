13dc8b0e318a8c2387bdc8c79f8420ac2e2ffb1a
Anton <ntncmch@gmail.com>
2013-04-18 09:16:27 +0100

    a .R file allowing for documentation of RData using roxygen2

---
+++ pkg/R/docRoxygenRData.R
@@ -0,0 +1,14 @@
+#epidase documentation for RData using roxygen2
+#put you .RData in the /data directory
+#add you roxygen2 description of your data below
+#run roxygenize("./pkg") in the root directory of the epibase package
+
+#' @name H1N1pdm2009
+#' @title Dataset from the 2009 influenza A/H1N1 pandemic
+#' @description This dataset is a list containing the following components:
+#' \item{\code{individuals}}{A dataframe which contains 514 individual ID as well as their location}
+#' \item{\code{samples}}{A dataframe which contains 514 individual ID, their sample ID and date as well as the ID of the associated genetic sequence}
+#' \item{\code{dna}}{A \code{\link{DNAbin}} object containing 514 genetic sequences of 2009/H1N1 Haemaglutinin}
+#' @author Anton Camacho
+
+
