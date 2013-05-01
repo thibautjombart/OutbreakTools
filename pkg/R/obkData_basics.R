5f25b51c26f1e75319d0b5c650cee043ba453d24
Anne Cori <annecori@wpia-didelt012.dide.ic.ac.uk>
2013-05-01 15:46:20 +0100

    corrected bugs in summary(obkData)

--- pkg/R/obkData_basics.R
+++ pkg/R/obkData_basics.R
@@ -38,14 +38,14 @@ setMethod("summary", "obkData", function(object, ...){
 
     cat(paste("Dataset of ",get.nindividuals(object,"all")," individuals with...\n",sep=""))
 
-    cat(paste("- ",get.nsamples(object)," samples, coming from ",get.nindividuals(object,"samples")," individuals",sep=""))
+    cat(paste("- ",get.nsamples(object)," samples \n\tcoming from ",get.nindividuals(object,"samples")," individuals",sep=""))
 
     if(!is.null(object@samples) && dim(object@samples)[2] <=13 )
     {
-        cat(paste(", collected between ",min(object@samples$date)," and ",max(object@samples$date),", regarding:\n",sep=""))
+        cat(paste("\n\tcollected between ",min(object@samples$date)," and ",max(object@samples$date),"\n \tcontaining information on:\n",sep=""))
         for (i in (1:dim(object@samples)[2])[-which((names(object@samples) %in% c("individualID","sampleID","date"))==TRUE)])
         {
-            cat(paste("\t\t ",names(object@samples)[i],"\t\t",sep=""))
+            cat(paste("\t\t ",names(object@samples)[i],"\t",sep=""))
             if(is.numeric(object@samples[,i])==TRUE){cat(paste("(mean: ",signif(mean((object@samples[,i]),na.rm=TRUE),digits=6),", sd: ",signif(sd((object@samples[,i]),na.rm=TRUE),digits=6),")",sep=""))}
             cat("\n")
         }
@@ -53,7 +53,7 @@ setMethod("summary", "obkData", function(object, ...){
 
 	if(!is.null(object@dna)) 
 	{
-		cat(paste("- ",get.nsequences(object)," corresponding sequences on ",get.nlocus(object)," loci (concacenated alignments: ",sum(sapply(object@dna@dna,ncol))," nucleotides)\n",sep=""))
+		cat(paste("- ",get.nsequences(object)," corresponding sequences on ",get.nlocus(object)," loci \n\t(concatenated alignments: ",sum(sapply(object@dna@dna,ncol))," nucleotides)\n",sep=""))
 	}else
 	{
 		cat("- 0 sequences\n")
@@ -62,7 +62,7 @@ setMethod("summary", "obkData", function(object, ...){
     cat(paste("- clinical data from ",get.nindividuals(object,"clinical")," individuals",sep=""))
     if(!is.null(object@clinical) && length(object@clinical) <=10 )
     {
-        cat(", regarding:\n")
+        cat("\n\t containing information on:\n")
         for (i in 1:length(object@clinical))
         {
             cat(paste("\t\t ",names(object@clinical)[i],"\n",sep=""))
@@ -71,12 +71,18 @@ setMethod("summary", "obkData", function(object, ...){
 
     if(!is.null(object@contacts))
     {
-        cat(paste("- ",get.ncontacts(object,"contacts")," contacts recorded between ",get.nindividuals(object,"contacts")," individuals\n",sep=""))
+        cat(paste("- ",get.ncontacts(object@contacts,"contacts")," contacts recorded between ",get.nindividuals(object,"contacts")," individuals\n",sep=""))
     }
 
     if(!is.null(object@trees))
     {
-        cat(paste("- ",length(object@trees)," phylogenetic trees with ",length(object@trees$tip.label)," tips\n",sep=""))
+		if(length(object@trees)>1)
+		{
+        	cat(paste("- ",length(object@trees)," phylogenetic trees with ",length(object@trees[[1]]$tip.label)," tips\n",sep=""))
+		}else
+		{
+			cat(paste("- ",length(object@trees)," phylogenetic tree with ",length(object@trees[[1]]$tip.label)," tips\n",sep=""))
+		}
     }
 
     return(invisible())
