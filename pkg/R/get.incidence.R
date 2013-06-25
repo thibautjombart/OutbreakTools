eadca3558e490ff8123ce0120fee2dddbdd5fb80
Thibaut Jombart <tjombart@imperial.ac.uk>
2013-06-25 18:27:34 +0100

    developping the function - not finished yet

--- pkg/R/get.incidence.R
+++ pkg/R/get.incidence.R
@@ -4,7 +4,34 @@
 ## get.incidence ##
 ###################
 
-setMethod("get.incidence", "obkData", function(x, ...){
+setMethod("get.incidence", "obkData", function(x, use=c("collection"), ...){
+    if(get.nsamples(x)<1) return(NULL)
+
+    ## get dates
+    dates <- get.dates(x, data="samples")
+    first.date <- min(dates <- get.dates(x, data="samples"))
+    last.date <- max(dates <- get.dates(x, data="samples"))
+    out.dates <- seq(first.date, last.date, by=1) # output dates
+
+    ## get vector of "positive outcomes" ##
+
+    ## split vector per individual ##
+
+    ## get date of first positive for each individual ##
+
+    ## auxiliary function: find 1st positive for one individual
+    f1 <- function(e){
+        ## no positive outcome
+        if() return(NULL)
+
+        ## at least one positive outcome
+        ## find first date
+        ## return
+        return(out)
+    }
+
+    ## fill in the output ##
+ 
 
 }) # end get.incidence
 
