fd9ccc07d6bf05a5fac71b36b3627a1f9b178b4d
Thibaut Jombart <tjombart@imperial.ac.uk>
2013-07-12 10:56:05 +0100

    new subset function for obkSequences - tested, works.

--- pkg/R/subset.R
+++ pkg/R/subset.R
@@ -4,9 +4,120 @@
 #### SUBSETTING PROCEDURES ####
 ###############################
 
-###################
+#########################
+## obkSequences method ##
+#########################
+setMethod("subset", "obkSequences", function(x, sequences=NULL, locus=NULL, individuals=NULL,
+                                        date.from=NULL, date.to=NULL, date.format=NULL, ...){
+    ## ESCAPE IF NOTHING IN THE DATASET ##
+    if(get.nsequences(x)==0) return(x)
+
+    ## SUBSET BY SEQUENCE ID ##
+    if(!is.null(sequences)){
+        ## handle non-character argument ##
+        if(!is.character(sequences)){
+            sequences <- get.sequences(x)[sequences]
+        }
+
+        ## check that all sequences are known ##
+        if(!all(sequences %in% get.sequences(x))){
+            temp <- paste(sequences[!sequences %in% get.sequences(x)], collapse=", ")
+            warning(paste("The following sequences were not found in the data:", temp))
+            sequences <- sequences[sequences %in% get.sequences(x)]
+        }
+
+        ## subset x@dna ##
+        x@dna <- get.dna(x, id=sequences)
+        
+        ## subset x@meta ##
+        x@meta <- x@meta[sequences,,drop=FALSE]
+
+    } # end subset by sequence ID
+
+
+    ## SUBSET BY LOCUS ##
+    if(!is.null(locus)){
+        ## handle non-character argument ##
+        if(!is.character(locus)){
+            locus <- get.locus(x)[locus]
+        }
+
+        ## check that all loci are known ##
+        if(!all(locus %in% get.locus(x))){
+            temp <- paste(locus[!locus %in% get.locus(x)], collapse=", ")
+            warning(paste("The following loci were not found in the data:", temp))
+            locus <- locus[locus %in% get.locus(x)]
+        }
+
+        ## subset x@dna ##
+        x@dna <- x@dna[locus]
+        seq.tokeep <- unlist(lapply(x@dna, rownames))
+
+        ## subset x@meta ##
+        x@meta <- x@meta[seq.tokeep,,drop=FALSE]
+
+    } # end subset by locus
+
+
+     ## SUBSET BY INDIVIDUAL ID ##
+    if(!is.null(individuals)){
+        ## handle non-character argument ##
+        if(!is.character(individuals)){
+            individuals <- get.individuals(x)[individuals]
+        }
+
+        ## check that all individuals are known ##
+        if(!all(individuals %in% get.individuals(x))){
+            temp <- paste(individuals[!individuals %in% get.individuals(x)], collapse=", ")
+            warning(paste("The following individuals were not found in the data:", temp))
+            individuals <- individuals[individuals %in% get.individuals(x)]
+        }
+
+        ## find sequences to retain ##
+        seq.tokeep <- rownames(x@meta)[x@meta$individualID %in% individuals]
+
+        ## subset by sequences ##
+        x <- subset(x, sequences=seq.tokeep)
+    } # end subset by individual ID
+
+
+    ## DATES FROM ... ##
+    if(!is.null(date.from)){
+        ## process date ##
+        date.from <- .process.Date(date.from, format=date.format)
+
+        ## find sequences to retain ##
+        seq.tokeep <- x@meta$date >= date.from
+
+        ## subset by sequences ##
+        x <- subset(x, sequences=seq.tokeep)
+    } # end subset by date.from
+
+
+    ## DATES TO ... ##
+    if(!is.null(date.to)){
+         ## process date ##
+        date.to <- .process.Date(date.to, format=date.format)
+
+        ## find sequences to retain ##
+        seq.tokeep <- x@meta$date <= date.to
+
+        ## subset by sequences ##
+        x <- subset(x, sequences=seq.tokeep)
+    } # end subset by date.to
+
+
+    return(x)
+}) # end subset for obkSequences
+
+
+
+
+
+
+####################
 ## obkData method ##
-###################
+####################
 setMethod("subset", "obkData", function(x, individuals=NULL, locus=NULL, sequences=NULL,
                                         date.from=NULL, date.to=NULL, date.format=NULL,
                                         row.individuals=NULL, row.samples=NULL,...){
@@ -14,7 +125,7 @@ setMethod("subset", "obkData", function(x, individuals=NULL, locus=NULL, sequenc
     if(is.null(x@individuals)) {
         individuals <- row.individuals <- NULL
     }
-  
+
     if(is.null(x@dna)){
         locus <- sequences <- NULL
     }
