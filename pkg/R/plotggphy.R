423faa6935cdcd486d0e65fad9bea9d92f940728
jombart <t.jombart@imperial.ac.uk>
2013-07-15 20:10:52 +0100

    Think I found what function of plyr is used (rename); renamed colour->color

--- pkg/R/plotggphy.R
+++ pkg/R/plotggphy.R
@@ -16,7 +16,7 @@ if(getRversion() >= "2.15.1")  utils::globalVariables(c("x.beg","x.end","y.beg",
 #' @param show.tip.label Logical. If \code{TRUE}, the labels of the tip are shown.
 #' @param tip.label.size Numeric. Size of the tip labels.
 #' @param build.tip.attribute Logical. If \code{TRUE}, then a data frame \code{tip.attribute} is constructed by merging the data frames \code{individuals} and \code{records}.
-#' @param tip.colour Character. Can be either the name of a color (e.g. "Black") or the name of a column of \code{tip.attribute}. In the first case, all tips have the specified color. In the second case, tips are color-codded according to the specified attribute.
+#' @param tip.color Character. Can be either the name of a color (e.g. "Black") or the name of a column of \code{tip.attribute}. In the first case, all tips have the specified color. In the second case, tips are color-codded according to the specified attribute.
 #' @param tip.alpha Character (or Numeric). Can be either the value of transparency (between 0 and 1) or the name of a column of \code{tip.attribute}. In the first case, all tips have the specified transparency. In the second case, tips are transparency-codded according to the specified attribute.
 #' @param tip.shape Character (or Numeric). Can be either the value of a shape (e.g. 16 correpond to filled circles) or the name of a column of \code{tip.attribute}. In the first case, all tips have the specified shape. In the second case, tips are shape-codded according to the specified attribute.
 #' @param tip.size Character (or Numeric). Can be either the value of tip size or the name of a column of \code{tip.attribute}. In the first case, all tips have the specified size. In the second case, tips are size-codded according to the specified attribute.
@@ -28,7 +28,7 @@ if(getRversion() >= "2.15.1")  utils::globalVariables(c("x.beg","x.end","y.beg",
 #' @param axis.date.format Character. When x-axis is in date format, this argument allow to change the format of the tick labels. See strptime for more details.
 #' @param major.breaks Character. Major x-axis breaks (only when x is in date format). Ex: "weeks", "15days", "months", etc.
 #' @param minor.breaks Character. Minor x-axis breaks (only when x is in date format). Ex: "weeks", "15days", "months", etc.
-#' @param colour.palette Character. The palette for tip colors. Only palettes from the package \code{\link{RColorBrewer}} are available. See \code{\link{brewer.pal}} documentation for more details.
+#' @param color.palette Character. The palette for tip colors. Only palettes from the package \code{\link{RColorBrewer}} are available. See \code{\link{brewer.pal}} documentation for more details.
 #' @param legend.position Character (or numeric). The position of legends. ("left", "right", "bottom", "top", or two-element numeric vector)
 #' @export
 #' @import ggplot2,RColorBrewer
@@ -36,9 +36,9 @@ if(getRversion() >= "2.15.1")  utils::globalVariables(c("x.beg","x.end","y.beg",
 #' @example ../misc/plotggphyExample.R
 #'
 plotggphy <- function(x, which.tree = 1, ladderize = FALSE, show.tip.label = FALSE, tip.label.size = 3, build.tip.attribute = FALSE,
-	tip.colour = NULL, tip.alpha = NULL, tip.shape = NULL, tip.size = NULL, branch.unit = NULL, tip.dates = NULL,
+	tip.color = NULL, tip.alpha = NULL, tip.shape = NULL, tip.size = NULL, branch.unit = NULL, tip.dates = NULL,
 	guess.tip.dates.from.labels = FALSE, set.guess = list(prefix = "_", order = 1, from = "last"), axis.date.format = NULL,
-                      major.breaks = NULL, minor.breaks = NULL, colour.palette = "Spectral", legend.position = "right") {
+                      major.breaks = NULL, minor.breaks = NULL, color.palette = "Spectral", legend.position = "right") {
 
     ##stop if:
     if (!inherits(x, "obkData")) {
@@ -60,9 +60,9 @@ plotggphy <- function(x, which.tree = 1, ladderize = FALSE, show.tip.label = FAL
         ##TODO: add other data frames (e.g. records, etc)
         df.individuals <- get.data(x, "individuals")
         df.individuals$individualID <- 1:nrow(df.individuals)
-        df.records <- get.data(x, "records")
+        df.dna <- x@dna@meta
 
-        tip.attribute <- merge(df.records, df.individuals, by = "individualID", all.x = TRUE)
+        tip.attribute <- merge(df.dna, df.individuals, by = "individualID", all.x = TRUE)
         tip.attribute <- rename(tip.attribute, c(sequenceID = "label"))
 
         if (!is.null(tip.dates)) {
@@ -77,9 +77,9 @@ plotggphy <- function(x, which.tree = 1, ladderize = FALSE, show.tip.label = FAL
             }
 
         }
-        if (!is.null(tip.colour)) {
-            if (!tip.colour %in% names(tip.attribute) & !is.color(tip.colour)) {
-                stop("tip.colour must be either the name of a column of tip.attribute or a colour name (e.g. \"red\").")
+        if (!is.null(tip.color)) {
+            if (!tip.color %in% names(tip.attribute) & !is.color(tip.color)) {
+                stop("tip.color must be either the name of a column of tip.attribute or a color name (e.g. \"red\").")
             }
         }
         if (!is.null(tip.shape)) {
@@ -101,9 +101,9 @@ plotggphy <- function(x, which.tree = 1, ladderize = FALSE, show.tip.label = FAL
 
         tip.attribute <- NULL
 
-        if (!is.null(tip.colour)) {
-            if (!is.color(tip.colour)) {
-                stop("tip.colour must be either the name of a column of tip.attribute or a colour name (e.g. \"red\").")
+        if (!is.null(tip.color)) {
+            if (!is.color(tip.color)) {
+                stop("tip.color must be either the name of a column of tip.attribute or a color name (e.g. \"red\").")
             }
         }
         if (!is.null(tip.shape)) {
@@ -191,7 +191,7 @@ plotggphy <- function(x, which.tree = 1, ladderize = FALSE, show.tip.label = FAL
     }
 
 
-    tip.characteristic <- list(colour = tip.colour, alpha = tip.alpha, shape = tip.shape, size = tip.size)
+    tip.characteristic <- list(color = tip.color, alpha = tip.alpha, shape = tip.shape, size = tip.size)
     is.aes <- sapply(seq_along(tip.characteristic), function(i) {
         x.val <- tip.characteristic[[i]]
         x.name <- names(tip.characteristic)[i]
@@ -202,7 +202,7 @@ plotggphy <- function(x, which.tree = 1, ladderize = FALSE, show.tip.label = FAL
         if (x.val %in% names(tip.attribute)) {
             return(T)
         }
-        if (x.name == "colour" & is.color(x.val)) {
+        if (x.name == "color" & is.color(x.val)) {
             return(F)
         }
         if (x.name %in% c("alpha", "shape", "size") & is.numeric(x.val)) {
@@ -231,19 +231,19 @@ plotggphy <- function(x, which.tree = 1, ladderize = FALSE, show.tip.label = FAL
                             ")", tip.fix.txt, ")")))
     }
 
-    if ("colour" %in% tip.aes) {
-        nMaxCol <- brewer.pal.info[colour.palette, "maxcolors"]
-        tmp <- df.tip[, tip.colour]
+    if ("color" %in% tip.aes) {
+        nMaxCol <- brewer.pal.info[color.palette, "maxcolors"]
+        tmp <- df.tip[, tip.color]
 
         if (is.discrete(tmp)) {
             if (length(unique(tmp)) > nMaxCol) {
-                warning(paste("too many tip colours for palette", colour.palette, ": we use default palette instead"))
-                p <- p + scale_colour_discrete(tip.colour)
+                warning(paste("too many tip colors for palette", color.palette, ": we use default palette instead"))
+                p <- p + scale_color_discrete(tip.color)
             } else {
-                p <- p + scale_colour_brewer(tip.colour, palette = colour.palette)
+                p <- p + scale_color_brewer(tip.color, palette = color.palette)
             }
         } else {
-            p <- p + scale_colour_gradientn(tip.colour, colours = brewer.pal(nMaxCol, colour.palette))
+            p <- p + scale_color_gradientn(tip.color, colors = brewer.pal(nMaxCol, color.palette))
         }
     }
 
