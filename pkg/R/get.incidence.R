7973eabc71f225a4518e940ce4c1fa065968d1aa
Thibaut Jombart <tjombart@imperial.ac.uk>
2013-10-16 15:33:14 +0100

    first version of get.incidence

--- pkg/R/get.incidence.R
+++ pkg/R/get.incidence.R
@@ -1,37 +1,61 @@
 
 
-## ###################
-## ## get.incidence ##
-## ###################
+###################
+## get.incidence ##
+###################
+setGeneric("get.incidence", function(x, ...) standardGeneric("get.incidence"))
 
+#################
+## Date method ##
+#################
+setMethod("get.incidence", "Date", function(x, first.date=NULL, last.date=NULL, interval=1, ...){
+    ## GET DATES OF THE OUTPUT ##
+    if(is.null(first.date)){
+        first.date <- min(x, na.rm=TRUE)
+    } else {
+        first.date <- min(min(x, na.rm=TRUE), first.date)
+    }
+    if(is.null(last.date)){
+        last.date <- max(x, na.rm=TRUE)
+    } else {
+        last.date <- max(max(x, na.rm=TRUE), last.date)
+    }
+    out.dates <- seq(first.date, last.date, by=interval) # output dates
+
+
+    ## COMPUTE INCIDENCE ##
+    ## incid is computed for time intervals
+    ## incid on interval [d1,d2[ is named after d1
+    breaks <- c(as.integer(out.dates), as.integer(last.date)+interval)
+    incid <- table(cut(as.integer(x), breaks=breaks, right=FALSE))
+    out <- data.frame(date=out.dates, incidence=as.integer(incid))
+    return(out)
+}) # end Date method
+
+
+
+
+
+#########################
+## obkSequences method ##
+#########################
+setMethod("get.incidence", "obkSequences", function(x, first.date=NULL, last.date=NULL, ...){
+    if(is.null(x) || get.nsequences(x)<1) return(NULL)
+
+    out <- get.incidence(x@meta$date)
+    return(out)
+
+}) # end get.incidence
+
+
+
+
+
+## ####################
+## ## obkData method ##
+## ####################
 ## setMethod("get.incidence", "obkData", function(x, use=c("collection"), ...){
 ##     if(get.nrecords(x)<1) return(NULL)
-
-##     ## get dates
-##     dates <- get.dates(x, data="records")
-##     first.date <- min(dates <- get.dates(x, data="records"))
-##     last.date <- max(dates <- get.dates(x, data="records"))
-##     out.dates <- seq(first.date, last.date, by=1) # output dates
-
-##     ## get vector of "positive outcomes" ##
-
-##     ## split vector per individual ##
-
-##     ## get date of first positive for each individual ##
-
-##     ## ## auxiliary function: find 1st positive for one individual
-##     ## f1 <- function(e){
-##     ##     ## no positive outcome
-##     ##     if() return(NULL)
-
-##     ##     ## at least one positive outcome
-##     ##     ## find first date
-##     ##     ## return
-##     ##     return(out)
-##     ## }
-
-##     ## fill in the output ##
-
 
 ## }) # end get.incidence
 
