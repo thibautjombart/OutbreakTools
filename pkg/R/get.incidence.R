071032d801acd0723d323843f9fd851c5e4fdeba
jombart <t.jombart@imperial.ac.uk>
2013-10-21 14:08:39 +0100

    first draft of get.incidence for obkData

--- pkg/R/get.incidence.R
+++ pkg/R/get.incidence.R
@@ -86,11 +86,83 @@ setMethod("get.incidence", "obkContacts", function(x, first.date=NULL, last.date
 
 
 
-## ####################
-## ## obkData method ##
-## ####################
-## setMethod("get.incidence", "obkData", function(x, use=c("collection"), ...){
-##     if(get.nrecords(x)<1) return(NULL)
+####################
+## obkData method ##
+####################
+##
+## based on 'dates' associated to a given field
+## 'values' are optional and can be used to subset the retained 'dates'
+## (e.g. define what a positive case is)
+setMethod("get.incidence", "obkData", function(x, data, where=NULL, val.min=NULL, val.max=NULL, val.kept=NULL, regexp=NULL,
+                                               first.date=NULL, last.date=NULL, interval=1, add.zero=TRUE, ...){
+    ## HANDLE ARGUMENTS ##
+    where <- match.arg(where)
+    if(is.null(val.min)) val.min <- -Inf
+    if(is.null(val.max)) val.max <- Inf
 
-## }) # end get.incidence
+
+    ## GET DATA ##
+    df <- get.data(x, data=data, where=where, showSource=TRUE)
+    if(is.null(df)) stop(paste("Data",data,"cannot be found in this obkData object"))
+
+    ## call specific procedures if applicable ##
+    if(inherits(df, c("obkSequences", "obkContacts"))) {
+        return(get.incidence(df, first.date=first.date, last.date=last.date,
+                             interval=interval, add.zero=add.zero))
+    }
+
+
+    ## OTHERWISE: DATA ASSUMED TAKEN FROM RECORDS ##
+    ## if data=='records', keep the first data.frame of the list ##
+    if(is.list(df) && !is.data.frame(df) && is.data.frame(df[[1]])) df <- df[[1]]
+
+    ## get dates ##
+    if(!date %in% names(df)) stop("no date in the data")
+    dates <- df$date
+
+    ## get optional values associated to the dates ##
+    ## keep 'data' if it is there
+    if(data %in% names(df)){
+        values <- df[[data]]
+    } else { ## else keep first optional field
+        temp <- !names(df) %in% c("individualID","date") # fields being not "individualID" or "date"
+        if(any(temp)) {
+            values <- df[,min(which(temp))]
+        } else {
+            values <- NULL
+        }
+    }
+
+
+    ## EXTRACT RELEVANT DATES ##
+    if(!is.null(values)){
+        toKeep <- rep(TRUE, length(values))
+
+        ## if 'values' is numeric ##
+        if(is.numeric(values)){
+            toKeep <- toKeep & (values>=val.min & values<=val.max)
+        }
+
+        ## if val.ok is provided ##
+        if(!is.null(val.ok)) {
+            toKeep <- toKeep & (values %in% val.ok)
+        }
+
+        ## if regexp is provided ##
+        if(!is.null(regexp)) {
+            temp <- rep(FALSE, length(values))
+            temp[grep(regexp, values, ...)] <- TRUE
+            toKeep <- toKeep & temp
+        }
+
+        dates <- dates[toKeep]
+    }
+
+    ## CALL THE DATE PROCEDURE ##
+    out <- get.incidence(dates, first.date=first.date, last.date=last.date,
+                         interval=interval, add.zero=add.zero)
+
+    ## RETURN OUTPUT ##
+    return(out)
+}) # end get.incidence
 
