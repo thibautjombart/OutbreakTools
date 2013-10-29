78c7ce9b3707c1bfac8bd67c81c8933bc0874210
Thibaut Jombart <tjombart@imperial.ac.uk>
2013-10-29 18:43:49 +0000

    Working on handling @records and @contacts from JSON format

--- pkg/R/importFromJSON.R
+++ pkg/R/importFromJSON.R
@@ -1,4 +1,27 @@
 JSON2obkData <- function(individuals=NULL, records=NULL, contacts=NULL, context=NULL){
+    ## AUXILIARY FUNCTIONS ##
+    ## function to fill in vectors so that they all have 'allfields' entries
+    f1 <- function(x){
+        out <- as.character(rep(NA, length(allfields)))
+        names(out) <- allfields
+        out[names(x)] <- x
+        return(out)
+    }
+
+    ## function removing all columns of a data.frame that are all NAs
+    f2 <- function(x){
+        toKeep <- sapply(x, function(e) !all(is.na(e)))
+        out <- x[,toKeep,drop=FALSE]
+        return(out)
+    }
+
+
+    ## INITIALIZE RESULTS ##
+    tabind <- NULL
+    listtabrec <- NULL
+    tabcontacts <- NULL
+
+
     ## EXTRACT INDIVIDUAL DATA ##
     if(!is.null(individuals)){
         ## convert from json to list
@@ -9,14 +32,7 @@ JSON2obkData <- function(individuals=NULL, records=NULL, contacts=NULL, context=
         temp <- lapply(datind, unlist, recursive=TRUE)
         allfields <- unique(unlist(lapply(temp, names)))
 
-        ## function to fill in vectors so that they all have 'allfields' entries
-        f1 <- function(x){
-            out <- as.character(rep(NA, length(allfields)))
-            names(out) <- allfields
-            out[names(x)] <- x
-            return(out)
-        }
-
+        ## get data into a data.frame
         tabind <- matrix(unlist(lapply(temp, f1)), nrow=length(datind), byrow=TRUE)
         tabind <- as.data.frame(tabind)
         names(tabind) <- allfields
@@ -32,9 +48,50 @@ JSON2obkData <- function(individuals=NULL, records=NULL, contacts=NULL, context=
     }
 
 
-    ## TODO: TREAT records, contacts, context ##
+    ## EXTRACT RECORDS DATA ##
+    if(!is.null(records)){
+        ## convert from json to list
+        datrec <- fromJSON(records)
+
+        ## get data into a data.frame ##
+        ## get all fields
+        temp <- lapply(datrec, unlist, recursive=TRUE)
+        allfields <- unique(unlist(lapply(temp, names)))
+
+        ## get data into a data.frame
+        tabrec <- matrix(unlist(lapply(temp, f1)), nrow=length(datrec), byrow=TRUE)
+        tabrec <- as.data.frame(tabrec)
+        names(tabrec) <- allfields
+
+        ## restore numerics where needed
+        tabrec <- data.frame(lapply(tabrec, .restoreNumericType))
+
+        ## restore dates were needed
+        areDates <- grep("date", names(tabrec), ignore.case=TRUE)
+        if(length(areDates)>1) for(i in areDates){
+            tabrec[[i]] <- .process.Date(tabrec[[i]])
+        }
+
+        ## get the final list for @records ##
+        ## get list
+        listtabrec <- split(tabrec, tabrec[,grep("choice",names(tabrec), ignore.case=TRUE)])
+
+        ## remove columns which are all NAs
+        listtabrec <- lapply(listtabrec,f2)
+
+
+        ## FILTER TABS FOR CONTACT INFORMATION ##
+        contact.info <- grep("contact", names(listtabrec), ignore.case=TRUE)
+        if(length(contact.info)>0){
+            tabcontacts <- listtabrec[[contact.info]]
+            listtabrec <- listtabrec[-contact.info]
+        }
+    }
+
+
+    ## TODO: TREAT contacts, context ##
 
     ## BUILD OBJECT AND RETURN ##
-    out <- new("obkData", individuals=tabind)
+    out <- new("obkData", individuals=tabind, records=listtabrec)
     return(out)
 }
